0561d1b625508bccf2caca08b284acc0fdd04171
Zihao <zihao@adoi.cn>
2022-07-01 22:39:43 +0800

    first commit

---
+++ src/components/Table.vue
@@ -0,0 +1,128 @@
+<template>
+  <div id="table"></div>
+</template>
+
+<script>
+export default {
+  methods: {
+    renderTable() {
+      this.$table({
+        data: [
+          { name: "小明", age: 20, sex: "男" },
+          { name: "小黄", age: 18, sex: "女"  },
+          { name: "小红", age: 16 , sex: "女" },
+          { name: "小蓝", age: 10, sex: "保密"  },
+          { name: "小白", age: 9 , sex: "男" },
+          { name: "小黑", age: 24, sex: "男"  },
+          { name: "tom", age: 19 , sex: "男" },
+          { name: "alex", age: 20, sex: "保密"  },
+          { name: "smith", age: 22, sex: "男"  },
+          { name: "jack", age: 32, sex: "男"  },
+          { name: "ash", age: 30, sex: "女"  },
+          { name: "king", age: 29, sex: "男"  },
+          { name: "大白", age: 16, sex: "男"  },
+          { name: "阿兰", age: 17 , sex: "保密" },
+          { name: "阿莱", age: 30 , sex: "男" },
+        ],
+        pagination: {
+          pageSize: [5, 10, 15],
+        },
+        columns: [
+          {
+            prop: "name",
+            title: "姓名",
+          },
+          {
+            prop: "age",
+            title: "年龄",
+          },
+          {
+            prop: "sex",
+            title: "性别",
+          },
+        ],
+      });
+    },
+  },
+  mounted() {
+    this.renderTable();
+  },
+};
+</script>
+
+<style lang="scss">
+.tableCom {
+  width: 500px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+table {
+  border-collapse: collapse;
+  border: 1px solid #ccc;
+  margin: 0 auto;
+  thead {
+    background: #eee;
+  }
+  td {
+    text-align: center;
+    width: 100px;
+    height: 30px;
+    line-height: 30px;
+    border: 1px solid #ccc;
+  }
+}
+.pagination {
+  margin: 15px auto 0;
+  display: flex;
+  justify-content: center;
+  .selectPageSize {
+    position: relative;
+    .pageSizeModal {
+      width: 60px;
+      position: absolute;
+      background: #eee;
+      top: 22px;
+      left: 0;
+      li {
+        height: 20px;
+        cursor: pointer;
+        text-align: center;
+        &.on,
+        &:hover {
+          color: tomato;
+        }
+      }
+    }
+  }
+}
+.paginate {
+  display: flex;
+  button {
+    width: 22px;
+    height: 22px;
+    cursor: pointer;
+    outline: none;
+    border: 1px solid #eee;
+  }
+  ul {
+    display: flex;
+    li {
+      width: 20px;
+      height: 20px;
+      line-height: 20px;
+      cursor: pointer;
+      text-align: center;
+      border: 1px solid #eee;
+      margin-right: 3px;
+      &.on {
+        color: tomato;
+      }
+    }
+  }
+}
+.showPageSizeModal {
+  cursor: pointer;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,109 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.use({
+  install(Vue) {
+    Vue.prototype.$table = function ({ columns, data, pagination }) {
+      let Com = Vue.extend({
+        data() {
+          return {
+            columns,
+            data,
+            pageList: pagination.pageSize,
+            dataList: [],
+            currentPage: 1,
+            total: 0,
+            pageSize: 5,
+            pageNum: 0,
+            showPageSizeModal: false
+          }
+        },
+        template: `
+        <div class="tableCom">
+          <table border="1">
+            <thead>
+              <tr>
+                <td v-for="(item, index) in columns" :key="index" :prop="item.prop">{{item.title}}</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, index) in dataList" :key="index">
+                <td>{{item.name}}</td>
+                <td>{{item.age}}</td>
+                <td>{{item.sex}}</td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="pagination">
+            <span>共{{total}}条，</span>
+            <div class="selectPageSize">
+              <ul class="pageSizeModal" v-if="showPageSizeModal">
+                <li v-for="(item, index) in pageList" :key="index" :class="pageSize===item?'on':''" @click="setPageSize(item)">{{item}}</li>
+              </ul>
+              <div
+                class="showPageSizeModal"
+                @click="setPageSizeModal" 
+                >
+                每页{{pageSize}}条，
+              </div>
+            </div>
+            <div class="paginate">
+              <button @click="changePage('dec')" :disabled="currentPage<=1">&lt;</button>
+              <ul>
+                <li v-for="(item, index) in pageNum" :key="index" :class="currentPage==index+1?'on':''" @click="changePage(item)">{{item}}</li>
+              </ul>
+              <button @click="changePage('inc')" :disabled="currentPage>=pageNum">&gt;</button>
+            </div>
+          </div>
+        </div>
+        `,
+        methods: {
+          // 获取数据总长度
+          getTotal() {
+            this.total = this.data.length
+          },
+          // 分页
+          getPagination() {
+            this.dataList = this.data.slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize);
+            this.pageNum = Math.ceil(this.total / this.pageSize)
+          },
+          // 设置分页条数模态框
+          setPageSizeModal() {
+            this.showPageSizeModal = true
+          },
+          // 设置分页条数
+          setPageSize(item) {
+            this.pageSize = item
+            this.showPageSizeModal = false
+            this.getPagination()
+          },
+          changePage(type) {
+            // 递减
+            if (type === 'dec') {
+              this.currentPage--
+            } else if (type === 'inc') {
+              // 递增
+              this.currentPage++
+            } else {
+              // 点击页码切换
+              this.currentPage = type
+            }
+            // 调用设置分页数据
+            this.getPagination()
+          }
+        },
+        mounted() {
+          // 默认加载获取数据总长度
+          this.getTotal()
+          // 默认加载获取分页数据
+          this.getPagination()
+        },
+      })
+      new Com().$mount(document.getElementById('table'));
+    }
+  }
+})
+
 new Vue({
   router,
   store,
